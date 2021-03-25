b4ed27d38817351e8a39b0cbc2ee4a9881dd6e21
pranaylobo <lobopranayk9@gmail.com>
2021-03-25 23:37:00 +0530

    version9

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-let PORT = process.env.PORT || 8080;
+let PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"))
